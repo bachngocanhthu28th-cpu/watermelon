d3baad739be4966b154ad0d1de4083afe1b2da34
bachngocanhthu28th-cpu <bachngocanhthu28th@gmail.com>
2025-10-27 14:28:14 +0800

    Add files via upload

--- dfc00030-bach's project.py
+++ dfc00030-bach's project.py
@@ -1,6 +1,814 @@
-import turtle 
+from turtle import *
+tom = Turtle()
+tom.shape('turtle')
+tom.color( 'black', 'black')
+tom.speed(1500)
 
-screen=turtle.screen()
-screen.setup(1.0,1.0)
+tom.goto(-1000,1000)
+tom.color('light blue','light blue')
+tom.begin_fill()
+for y in range (4):
+    tom.fd(100000)
+    tom.rt(90)
+tom.end_fill()
 
 
+
+
+def i1():
+    tom.color('#008245','#008245')
+    tom.begin_fill()
+    for y in range(4):
+        tom.fd(40)
+        tom.lt(90)
+    tom.end_fill()
+
+
+def i2():
+    tom.color('#718200','#718200')
+    tom.begin_fill()
+    for y in range(4):
+        tom.fd(40)
+        tom.lt(90)
+    tom.end_fill()
+
+def i3():
+    tom.color('#FF957B','#FF957B')
+    tom.begin_fill()
+    for y in range(4):
+        tom.fd(40)
+        tom.lt(90)
+    tom.end_fill()
+
+
+def i4():
+    tom.color('#D90400','#D90400')
+    tom.begin_fill()
+    for y in range(4):
+        tom.fd(40)
+        tom.lt(90)
+    tom.end_fill()
+
+def i5():
+    tom.color('black','black')
+    tom.begin_fill()
+    for y in range (4):
+      tom.fd(40)
+      tom.lt(90)
+    tom.end_fill()
+
+tom.penup()
+tom.goto(-150,-350)
+tom.pendown()
+i1()
+
+
+tom.penup()
+tom.goto(-110,-350)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(-70,-350)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(-30,-350)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(10,-350)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(90,-310)
+tom.pendown()
+i1()
+tom.penup()
+tom.goto(50,-310)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(10,-310)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(-30,-310)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(-70,-310)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(-110,-310)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(-150,-310)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(-190,-310)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(-230,-310)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(-270,-270)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(-230,-270)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(-190,-270)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(-150,-270)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(-110,-270)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(-70,-270)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(-30,-270)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(10,-270)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(50,-270)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(90,-270)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(130,-270)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(130,-230)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(170,-230)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(90,-230)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(50,-230)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(10,-230)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(-30,-230)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-70,-230)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-110,-230)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-150,-230)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-190,-230)
+tom.pendown()
+i3()
+
+
+tom.penup()
+tom.goto(-230,-230)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(-270,-230)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(-230,-190)
+tom.pendown()
+i4()
+
+
+tom.penup()
+tom.goto(-190,-190)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-150,-190)
+tom.pendown()
+i5()
+
+tom.penup()
+tom.goto(-110,-190)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-70,-190)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-30,-190)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(10,-190)
+tom.pendown()
+i4()
+
+
+tom.penup()
+tom.goto(50,-190)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(90,-190)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(130,-190)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(170,-190)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(170,-150)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(210,-150)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(130,-150)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(90,-150)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(50,-150)
+tom.pendown()
+i4()
+
+
+tom.penup()
+tom.goto(10,-150)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-30,-150)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-70,-150)
+tom.pendown()
+i5()
+
+tom.penup()
+tom.goto(-110,-150)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-150,-150)
+tom.pendown()
+i4()
+
+
+tom.penup()
+tom.goto(-190,-150)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-150,-110)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-110,-110)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-70,-110)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-30,-110)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(10,-110)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(50,-110)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(90,-110)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(130,-110)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(170,-110)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(210,-110)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(210,-70)
+tom.pendown()
+i1()
+
+
+tom.penup()
+tom.goto(170,-70)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(130,-70)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(90,-70)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(50,-70)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(10,-70)
+tom.pendown()
+i5()
+
+tom.penup()
+tom.goto(-30,-70)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-70,-70)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-110,-70)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-70,-30)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-30,-30)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(10,-30)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(50,-30)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(90,-30)
+tom.pendown()
+i4()
+
+
+tom.penup()
+tom.goto(130,-30)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(170,-30)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(210,-30)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(210,10)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(170,10)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(130,10)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(90,10)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(50,10)
+tom.pendown()
+i5()
+
+tom.penup()
+tom.goto(10,10)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(-30,10)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(10,50)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(50,50)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(90,50)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(130,50)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(170,50)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(210,50)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(170,90)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(130,90)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(90,90)
+tom.pendown()
+i3()
+
+tom.penup()
+tom.goto(50,90)
+tom.pendown()
+i4()
+
+tom.penup()
+tom.goto(90,130)
+tom.pendown()
+i2()
+
+tom.penup()
+tom.goto(130,130)
+tom.pendown()
+i1()
+
+tom.penup()
+tom.goto(-300,0)
+def b():
+    tom.color('black','black')
+    tom.begin_fill()
+    for y in range (4):
+        tom.fd(20)
+        tom.lt(90)
+    tom.end_fill()
+def y():
+    tom.color('yellow','yellow')
+    tom.begin_fill()
+    for y in range (4):
+        tom.fd(20)
+        tom.rt(90)
+    tom.end_fill()
+
+
+
+tom.color('yellow')
+def line(x, y, n, s):
+    tom.penup()
+    tom.goto(x,y)
+    tom.pendown()
+    
+    
+    
+    for count in range(n):
+        tom.forward(s)
+        tom.backward(s)
+        tom.left(360/n)
+
+line(-300,300,100,100)
+
+
+
+
+def o():
+    tom.color("#ec8507",'#ec8507')
+    tom.begin_fill()
+    for y in range(4):
+        tom.fd(40)
+        tom.lt(90)
+    tom.end_fill()
+
+def r():
+    tom.color("#ee0505","#ee0505")
+    tom.begin_fill()
+    for y in range(4):
+        tom.fd(40)
+        tom.lt(90)
+    tom.end_fill()
+
+def lg():
+    tom.color("#389e34","#389e34")
+    tom.begin_fill()
+    for y in range(4):
+        tom.fd(40)
+        tom.lt(90)
+    tom.end_fill()
+
+
+def dg():
+    tom.color("#194917","#194917")
+    tom.begin_fill()
+    for y in range(4):
+        tom.fd(40)
+        tom.lt(90)
+    tom.end_fill()
+
+
+def y():
+    tom.color("#CBEC0F","#CBEC0F")
+    tom.begin_fill()
+    for y in range(4):
+        tom.fd(40)
+        tom.lt(90)
+    tom.end_fill()
+
+tom.penup()
+tom.goto(-700,-350)
+
+dg()
+tom.goto(-740,-350)
+dg()
+tom.goto(-780,-350) 
+
+
+dg() 
+tom.penup()
+tom.goto(-820,-310)
+tom.pendown()
+dg()
+tom.goto(-780,-310)
+lg()
+tom.goto(-740,-310)
+dg()
+tom.goto(-700,-310)
+dg()
+tom.goto(-660,-310)
+dg()
+tom.penup()
+tom.goto(-620,-270)
+tom.pendown()
+lg()
+tom.goto(-660,-270)
+dg()
+tom.penup()
+tom.goto(-740,-270)
+dg()
+tom.penup()
+tom.goto(-820,-270)
+lg()
+tom.penup()
+tom.goto(-860,-230)
+tom.pendown()
+lg()
+tom.goto(-820,-230)
+lg()
+tom.penup()
+tom.goto(-740,-230)
+tom.pendown()
+dg()
+tom.penup()
+tom.goto(-660,-230)
+lg()
+tom.goto(-620,-230)
+lg()
+tom.penup()
+tom.goto(-580,-190)
+lg()
+tom.goto(-620,-190)
+lg()
+
+tom.penup()
+tom.goto(-700,-190)
+tom.pendown()
+r()
+tom.goto(-740,-190)
+r()
+tom.goto(-780,-190)
+r()
+tom.penup()
+tom.goto(-860,-190)
+tom.pendown()
+lg()
+tom.penup()
+tom.goto(-820,-150)
+r()
+tom.goto(-780,-150)
+r()
+tom.goto(-740,-150)
+o()
+tom.goto(-700,-150)
+r()
+tom.goto(-660,-150)
+r()
+tom.penup()
+tom.goto(-580,-150)
+tom.pendown()
+lg()
+tom.penup()
+tom.goto(-660,-110)
+tom.pendown()
+r()
+tom.goto(-700,-110)
+o()
+tom.goto(-740,-110)
+o()
+tom.goto(-780,-110)
+o()
+tom.goto(-820,-110)
+r()
+tom.penup()
+tom.goto(-860,-70)
+tom.pendown()
+o()
+tom.goto(-820,-70)
+o()
+tom.goto(-780,-70)
+o()
+tom.goto(-740,-70)
+o()
+tom.goto(-700,-70)
+o()
+tom.goto(-660,-70)
+o()
+tom.goto(-620,-70)
+o()
+tom.penup()
+tom.goto(-620,-30)
+tom.pendown()
+o()
+tom.goto(-660,-30)
+o()
+tom.penup()
+tom.goto(-740,-30)
+tom.pendown()
+y()
+tom.penup()
+tom.goto(-820,-30)
+o()
+tom.goto(-860,-30)
+o()
+tom.penup()
+tom.goto(-820,10)
+tom.pendown()
+o()
+tom.penup()
+tom.goto(-740,10)
+tom.pendown()
+y()
+tom.penup()
+tom.goto(-660,10)
+tom.pendown()
+o()
+
+
+
+
+
+
+done()
